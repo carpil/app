2f9757ce400d3c9eb0f336a578261d409a884008
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-12-16 13:38:02 -0600

    🔧 [CARPIL-3] eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  ignorePatterns: ['/dist/*'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
+  },
+}
