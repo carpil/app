7249b4baaf95735f89988b02ae7001be042782ba
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-12-18 12:54:55 -0600

    🍱 [CARPIL-8] car, messages and profile icons

---
+++ components/icons.tsx
@@ -0,0 +1,16 @@
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+interface IconProps {
+  color: string
+}
+export const CarIcon = ({ color }: IconProps) => (
+  <Ionicons name="car" size={24} color={color} />
+)
+
+export const MessagesIcon = ({ color }: IconProps) => (
+  <Ionicons name="chatbox" size={24} color={color} />
+)
+
+export const ProfileIcon = ({ color }: IconProps) => (
+  <Ionicons name="person" size={24} color={color} />
+)
