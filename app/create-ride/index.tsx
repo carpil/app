26ce3b8d656764376ccd93fbb78a2ebe40ef8761
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-01-11 12:47:13 -0600

    ✨ [CARPIL-9] create ride button

---
+++ app/create-ride/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import Screen from '../../components/screen'
+// import MapView from 'react-native-maps'
+
+export default function CreateRide() {
+  return (
+    <Screen>
+      <Text>Create Ride</Text>
+      {/* <MapView style={styles.map} userInterfaceStyle={'dark'} /> */}
+    </Screen>
+  )
+}
