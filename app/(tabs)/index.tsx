d867f581d381415c6a0a9a150e98f6b493f75631
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-03-12 11:34:41 -0600

    💥 [CARPIL-23] get rides from service

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,23 @@
+import { useEffect, useState } from 'react'
+import { getRides } from 'services/api/rides'
+import { Ride } from '~types/ride'
 import { ScrollView } from 'react-native'
-import { rideRequests } from '@utils/mocks/ride-requests'
-import { rides } from '@utils/mocks/rides'
-import RideCard from '@components/ride-card'
-import RideRequestCard from '@components/ride-request-card'
 import CreateRideButton from '@components/create-ride-button'
+import RideCard from '@components/ride-card'
 import SafeScreen from '@components/safe-screen'
 
 export default function Index() {
+  const [rides, setRides] = useState<Ride[]>([])
+
+  useEffect(() => {
+    getRides().then(setRides)
+  }, [])
+
   return (
     <SafeScreen>
       <ScrollView>
         {rides.map((ride) => (
           <RideCard ride={ride} key={ride.id} />
-        ))}
-        {rideRequests.map((rideRequest) => (
-          <RideRequestCard ride={rideRequest} key={rideRequest.id} />
         ))}
       </ScrollView>
       <CreateRideButton />
