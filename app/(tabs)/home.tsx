90f4ce1e16a785fe277183d3704f9aa9fb1e358e
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-05-21 15:58:15 -0600

    🛂 [CARPIL-32] show user info

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,11 +2,14 @@ import { Text } from 'react-native'
 import PrimaryButton from '@components/buttons/primary'
 import SafeScreen from '@components/safe-screen'
 import { getAuth, signOut } from '@react-native-firebase/auth'
+import { useAuthStore } from 'store/useAuthStore'
 
 export default function Home() {
-  const onLogout = () => {
+  const logout = useAuthStore((state) => state.logout)
+  const onLogout = async () => {
     const auth = getAuth()
-    signOut(auth).then(() => console.log('User signed out!'))
+    await signOut(auth)
+    logout()
   }
   return (
     <SafeScreen>
