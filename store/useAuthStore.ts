a48f3b32c609fca2677a9f623d134be8e460c69d
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-05-21 15:56:22 -0600

    🛂 [CARPIL-32] auth store

---
+++ store/useAuthStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persist } from 'zustand/middleware'
+import { User } from '~types/user'
+
+type AuthState = {
+  user: User | null
+  token: string | null
+  login: (user: User, token: string) => void
+  logout: () => void
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      login: (user, token) => set({ user, token }),
+      logout: () => set({ user: null, token: null }),
+    }),
+    {
+      name: 'auth-storage',
+      storage: {
+        getItem: async (name) => {
+          const value = await AsyncStorage.getItem(name)
+          return value ? JSON.parse(value) : null
+        },
+        setItem: async (name, value) => {
+          await AsyncStorage.setItem(name, JSON.stringify(value))
+        },
+        removeItem: async (name) => {
+          await AsyncStorage.removeItem(name)
+        },
+      },
+    },
+  ),
+)
