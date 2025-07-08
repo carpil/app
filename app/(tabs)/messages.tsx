eb4fb5b7eee126f49b7a9b7aa71cb7b44d101d31
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-07-08 15:15:41 -0600

    📦️ [CARPIL-46] ios build

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,10 +1,28 @@
-import { Text } from 'react-native'
+import { ScrollView } from 'react-native'
 import SafeScreen from '@components/safe-screen'
+import RideCard from '@components/ride-card'
+import { Ride } from '~types/ride'
+import { getRides } from 'services/api/rides'
+import { useEffect, useState } from 'react'
+import CreateRideButton from '@components/create-ride-button'
 
 export default function Messages() {
+  const [rides, setRides] = useState<Ride[]>([])
+
+  useEffect(() => {
+    getRides().then((rides) => {
+      setRides(rides)
+    })
+  }, [])
+
   return (
     <SafeScreen>
-      <Text>Messages</Text>
+      <ScrollView>
+        {rides.map((ride) => (
+          <RideCard ride={ride} key={ride.id} />
+        ))}
+      </ScrollView>
+      <CreateRideButton />
     </SafeScreen>
   )
 }
