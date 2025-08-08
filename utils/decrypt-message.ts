c2c9154714845d427664a00bc39773386a0f5d26
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-08-08 10:53:13 -0600

    🛂 [CARPIL-41] decryptMessage

---
+++ utils/decrypt-message.ts
@@ -0,0 +1,13 @@
+import CryptoJS from 'crypto-js'
+
+const secretKey = process.env.EXPO_PUBLIC_CHAT_SECRET_KEY ?? ''
+
+export const decryptMessage = (ciphertext: string): string => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey)
+    return bytes.toString(CryptoJS.enc.Utf8)
+  } catch (error) {
+    console.error('Error decrypting message:', error)
+    return ciphertext
+  }
+}
