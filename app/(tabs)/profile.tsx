1827b1cf9b54b0cd684d8bfa1ff11b5088f7221c
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-08-27 13:29:22 -0600

    🔥 [CARPIL-50] provisional home tab

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,28 @@
 import { Text } from 'react-native'
 import SafeScreen from '@components/safe-screen'
 import { useAuthStore } from 'store/useAuthStore'
+import { getAuth, signOut } from '@react-native-firebase/auth'
+import { router } from 'expo-router'
+import PrimaryButton from '@components/buttons/primary'
 
 export default function Profile() {
   const user = useAuthStore((state) => state.user)
+
+  const logout = useAuthStore((state) => state.logout)
+  const onLogout = async () => {
+    const auth = getAuth()
+    await signOut(auth)
+    logout()
+    router.replace('/login')
+  }
 
   return (
     <SafeScreen>
       <Text>{user?.id}</Text>
       <Text>{user?.name}</Text>
       <Text>{user?.email}</Text>
+
+      <PrimaryButton text="Logout" onPress={onLogout} />
     </SafeScreen>
   )
 }
