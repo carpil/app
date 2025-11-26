9cfcb9d92a3aa9498ffec3c182760667426a7b8b
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2025-11-26 10:24:45 -0600

    🐛 [carpil-100] keyboard in scrollview

--- app/create-ride/select-origin.tsx
+++ app/create-ride/select-origin.tsx
@@ -15,7 +15,6 @@ export default function SelectOrigin() {
     <SafeScreen
       backgroundColor={COLORS.dark_gray}
       applyTopInset={false}
-      keyboardAware
     >
       <View style={{ flex: 1, paddingBottom: 10 }}>
         <PlacesAutocomplete
